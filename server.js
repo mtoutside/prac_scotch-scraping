5317259ca21936faa7b7a81bb5b8edf1756ae3f0
mtoutisde <more.humor.or@gmail.com>
2018-07-04 01:24:51 +0900

    edit server.js

--- server.js
+++ server.js
@@ -13,4 +13,12 @@ app.use(logger('dev'));
 // start the server
 app.listen(port, () => console.log(`App started on port ${port}.`));
 
+// Require the needed functions
+const { sendResponse } = require('./app/helpers');
+const { fetchAuthorProfile } = require('./app/scotch');
 
+// Add the Scotch author profile route
+app.get('/scotch/:author', (req, res, next) => {
+  const author = req.params.author;
+  sendResponse(res)(fetchAuthorProfile(author));
+});
